a518c9bc922bbfdd7061d2a31923b5f46bf90f1a
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-02 16:09:49 +0900

    Test

--- day190402/ex11.py
+++ day190402/ex11.py
@@ -28,15 +28,20 @@ import numpy as np
 # print(c)
 
 # My ver.
-d = np.zeros([5, 5], dtype=np.int32)
-d[[0, 1, 2, 3, 4], [0, 1, 2, 3, 4]] = 1
-print(d)
+# d = np.zeros([5, 5], dtype=np.int32)
+# d[[0, 1, 2, 3, 4], [0, 1, 2, 3, 4]] = 1
+# print(d)
 
+# My ver.
 e = [1, 3, 0, 3, 1]
 f = np.zeros([len(e), max(e)+1], dtype=np.int32)
 
 for ef in range(len(e)):
     f[ef, e[ef]] = 1
 
+print(f)
+
+# Teacher ver.
+f[[range(len(e))], e] = 1
 print(f)
 
