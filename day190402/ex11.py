4b75b617ac59ff1b759fd110a5a4895de2f4dcf7
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-02 15:58:58 +0900

    Test

--- day190402/ex11.py
+++ day190402/ex11.py
@@ -16,14 +16,27 @@ import numpy as np
 #
 # print(a[[1, 0], [0, 1]])    # 행, 행, 열, 열 순서!
 
-# My ver.
-b = np.ones([5, 5], dtype=np.int32)
-b[[1, 2, 3], 1:4] = 0
-print(b)
+# # My ver.
+# b = np.ones([5, 5], dtype=np.int32)
+# b[[1, 2, 3], 1:4] = 0
+# print(b)
+#
+# # Teacher ver.
+# c = np.zeros([5, 5], dtype='int32')
+# c[:, [0, -1]] = 1
+# c[[0, -1], :] = 1
+# print(c)
 
-# Teacher ver.
-c = np.zeros([5, 5], dtype='int32')
-c[:, [0, -1]] = 1
-c[[0, -1], :] = 1
-print(c)
+# My ver.
+# d = np.zeros([5, 5], dtype=np.int32)
+# d[[0, 1, 2, 3, 4], [0, 1, 2, 3, 4]] = 1
+# print(d)
+
+e = [1, 3, 0, 3, 1]
+f = np.zeros([len(e), max(e)+1], dtype=np.int32)
+
+for ef in range(len(e)):
+    f[ef, e[ef]] = 1
+
+print(f)
 
