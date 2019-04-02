59731445afff1c45437d5f87248fe5c8f8ce6f13
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-02 18:21:07 +0900

    Test

---
+++ day190402/matplotlibTest.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# data = [10, 20, 30, 40, 50]
+# plt.plot(data)
+# plt.show()
+
+x = [1, 2, 3, 4, 5]
+y = np.array(x) ** 2
+plt.plot(x, y)
+plt.show()
+
