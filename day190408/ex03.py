45b6b1fc382baabb6b50b3fe063ff8f987f3aea6
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-08 12:04:31 +0900

    Test

--- day190408/ex03.py
+++ day190408/ex03.py
@@ -3,14 +3,14 @@ import bitUtil
 df = bitUtil.getMovies()
 
 r_mean = df.pivot_table(values='rating', index='gender', aggfunc='mean')
-print(r_mean)
-
 r_mean2 = r_mean.unstack()      # index → columns
+
+print(r_mean)
 print(r_mean2)
 
 r1 = df.pivot_table(values='rating', index=['gender', 'age'], aggfunc='mean')
-print(r1)
-
 r2 = r1.unstack()
+
+print(r1)
 print(r2)
 
