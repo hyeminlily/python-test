1d3859f4b314c07089f4ea6af1d27ae425bdc034
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-08 13:01:51 +0900

    Test

--- day190408/bitUtil.py
+++ day190408/bitUtil.py
@@ -22,10 +22,10 @@ def get_500_movie():
     count = df.pivot_table(values='rating', index='title', aggfunc='count')
 
     # 평가 건수가 500개 이상인 영화 제목을 출력
-    # print(count['rating'] >= 500)     # boolean 값을 반환
+    print(count['rating'] >= 500)     # boolean 값을 반환
 
     title_500 = count[count['rating'] >= 500]
-    # print(title_500)
+    print(title_500)
 
     # 내림차순 정렬
     title_500_sort = title_500.sort_values(by='rating', ascending=False)
