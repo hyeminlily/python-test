3b50938d24f7bf8ebdc645d10dabf4bc3086e4c3
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-08 18:05:34 +0900

    Test

--- day190408/ex05.py
+++ day190408/ex05.py
@@ -8,14 +8,20 @@ cnt_500 = bitUtil.get_500_movie()
 # print(cnt_500.index)        # title
 # print(cnt_500.columns)      # rating
 
-# a = df.pivot_table(values='rating', index='title', columns='gender', fill_value=0, aggfunc='mean')
-# b = a.loc[cnt_500.index]
+a = df.pivot_table(values='rating', index='title', columns='gender', fill_value=0, aggfunc='mean')
+b = a.loc[cnt_500.index]
 # print(b)
 # print(len(cnt_500))
 
-a = df.pivot_table(values='rating', index='title', columns='gender', fill_value=0, aggfunc='mean')
-b = a.loc[cnt_500.index]
-b['diff'] = (b['F'] - b['M']).abs()
-c = b.sort_values(by='diff', ascending=False)
+# b['diff'] = (b['F'] - b['M']).abs()
+# c = b.sort_values(by='diff', ascending=False)
+# print(c.head())
+
+b['mdiff'] = b['M'] - b['F']
+c = b.sort_values(by='mdiff', ascending=False)
+print(c.head())
+
+b['mdiff'] = b['M'] - b['F']
+c = b.sort_values(by='mdiff', ascending=False)
 print(c.head())
 
