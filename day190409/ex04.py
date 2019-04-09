25e7300c92be119a0477ca6e32d925ffd79a5708
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-09 13:00:01 +0900

    Test

---
+++ day190409/ex04.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+
+file = open("by_mun.txt")
+text = file.read()
+file.close()
+
+font_path = 'C:/Users/Reina/BareunDotum1.ttf'
+cloud = WordCloud(font_path=font_path, background_color='white', width=800, height=600).generate(text)
+plt.imshow(cloud)
+plt.axis('off')
+plt.show()
+
