9a27ddfd6ece2d0d6c9037807eb1954e73c96958
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-09 15:43:09 +0900

    Test

--- day190409/ex04.py
+++ day190409/ex04.py
@@ -7,7 +7,8 @@ file.close()
 
 font_path = 'C:/Users/Reina/BareunDotum1.ttf'
 cloud = WordCloud(font_path=font_path, background_color='white', width=800, height=600).generate(text)
+cloud.words_
+
 plt.imshow(cloud)
 plt.axis('off')
 plt.show()
-
