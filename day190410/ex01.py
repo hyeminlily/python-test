c11fdc1abfbe01326cb6941e36ac9be5768f076e
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-10 17:54:00 +0900

    Test

--- day190410/ex01.py
+++ day190410/ex01.py
@@ -8,7 +8,7 @@ df = pd.read_excel("MLB World Series Champions_ 1903-2016.xlsx")
 # print(df.columns)
 #
 # # 팀별로 우승한 횟수 출력
-# win_cnt = df.pivot_table(values='Wins', index='Champion', aggfunc='count')
+win_cnt = df.pivot_table(values='Wins', index='Champion', aggfunc='count')
 # print(win_cnt)
 #
 # # 우승팀들의 평균 승률 출력
@@ -40,23 +40,22 @@ df = pd.read_excel("MLB World Series Champions_ 1903-2016.xlsx")
 #
 # print(nyy_win['Year'].min())
 # print(nyy_win['Year'].max())
-
+#
 # # New York Yankees의 평균 승률 출력
 # nyy = df[df['Champion'] == 'New York Yankees']
 # nyy_ratio = nyy.pivot_table(values='WinRatio', index='Champion', aggfunc='mean')
 # print(nyy_ratio)
+#
+# # 월드시리즈 열리지 않는 연도 출력
+# x = np.arange(int(df['Year'].min()), int(df['Year'].max()+1))
+# notYear = list(set(list(x)) - set(list(df['Year'])))
+# print(notYear)
+#
+# a = np.arange(int(df['Year'].min()), int(df['Year'].max()+1))
+# b = np.array(df['Year'])
+# notYear_byT = np.setdiff1d(a, b)
+# print(notYear_byT)
 
-# 최다 우승 Top5 출력
-# top5 = win_cnt.sort_values(by='Wins', ascending=False)
-# print(top5.head())
-
-# 월드시리즈 열리지 않는 연도 출력
-x = np.arange(int(df['Year'].min()), int(df['Year'].max()+1))
-notYear = list(set(list(x)) - set(list(df['Year'])))
-print(notYear)
-
-a = np.arange(int(df['Year'].min()), int(df['Year'].max()+1))
-b = np.array(df['Year'])
-notYear_byT = np.setdiff1d(a, b)
-print(notYear_byT)
-
+# 최다 우승 Top5 출력, 순위가 동일한 여러 팀도 모두 출력
+top5 = win_cnt.sort_values(by='Wins', ascending=False)
+print(top5.head())
