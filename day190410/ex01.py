93176644b6e72227c14ed26d928394781ae61657
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-10 10:51:45 +0900

    Test

--- day190410/ex01.py
+++ day190410/ex01.py
@@ -6,20 +6,24 @@ df = pd.read_excel("MLB World Series Champions_ 1903-2016.xlsx")
 # print(df)
 # print(df.head())
 # print(df.columns)
-
+#
 # # 팀별로 우승한 횟수 출력
 # win_cnt = df.pivot_table(values='Wins', index='Champion', aggfunc='count')
 # print(win_cnt)
-
+#
 # # 우승팀들의 평균 승률 출력
-win_ratio = df.pivot_table(values='WinRatio', index='Champion', aggfunc='mean')
+# win_ratio = df.pivot_table(values='WinRatio', index='Champion', aggfunc='mean')
 # print(win_ratio)
-
-# 평균 승률이 높은 상위 5개 팀 출력
-top5_mean = win_ratio.sort_values(by='WinRatio', ascending=False)
-print(top5_mean.head())
+#
+# # 평균 승률이 높은 상위 5개 팀 출력
+# top5_mean = win_ratio.sort_values(by='WinRatio', ascending=False)
+# print(top5_mean.head())
 
 # 2000년 이후의 데이터 중 평균 승률이 가장 높은 상위 5개 팀 출력
+win_2000 = df[df['Year'] >= 2000]
+mean_2000 = win_2000.pivot_table(values='WinRatio', index='Champion', aggfunc='mean')
+top5_2000 = mean_2000.sort_values(by='WinRatio', ascending=False)
+print(top5_2000.head())
 
 # 100승 이상 승리한 팀 출력
 # win_100 = df[df['Wins'] >= 100]
