c9e487f0abc7f14c63a45292846d887e55abe53e
hyeminlily <49010918+hyeminlily@users.noreply.github.com>
2019-04-10 10:45:34 +0900

    Test

--- day190410/ex01.py
+++ day190410/ex01.py
@@ -3,13 +3,13 @@ import pandas as pd
 import xlrd
 
 df = pd.read_excel("MLB World Series Champions_ 1903-2016.xlsx")
-print(df)
-print(df.head())
-print(df.columns)
+# print(df)
+# print(df.head())
+# print(df.columns)
 
 # 팀별로 우승한 횟수 출력
-# win_cnt = df.pivot_table(values='Wins', index='Champion', aggfunc='count')
-# print(win_cnt)
+win_cnt = df.pivot_table(values='Wins', index='Champion', aggfunc='count')
+print(win_cnt)
 
 # 우승팀들의 평균 승률 출력
 # win_ratio = df.pivot_table(values='WinRatio', index='Champion', aggfunc='mean')
